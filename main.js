f726ac975d6dc8c801a412c67d29ce533f4f7051
0n3W4y <vulturetm@gmail.com>
2017-03-14 20:46:05 +0300

    sync

--- main.js
+++ main.js
@@ -10,3 +10,43 @@ function initGame(){
 	game.init( creaturesData, humanoidsData, humanoidsClassData, humanoidsHelperData, lb, rb, journal, hb, el );
 	game.start();
 }
+
+function toHeroCreater(){
+	var battleContainer = document.getElementById( "batlle-wrapper" ).style.display = "none";
+	var heroCreater = document.getElementById( "hero-creater" ).style.display = "block";
+	var inventoryWrapperContainer = document.getElementById( "inventory-wrapper" ).style.display = "none";
+	var shopWrapperContainer = document.getElementById( "shop-wrapper" ).style.display = "none";
+	var welcomeContainer = document.getElementById( "welcome" ).style.display = "none";
+}
+
+function toWelcome(){
+	var battleContainer = document.getElementById( "batlle-wrapper" ).style.display = "none";
+	var heroCreater = document.getElementById( "hero-creater" ).style.display = "none";
+	var inventoryWrapperContainer = document.getElementById( "inventory-wrapper" ).style.display = "none";
+	var shopWrapperContainer = document.getElementById( "shop-wrapper" ).style.display = "none";
+	var welcomeContainer = document.getElementById( "welcome" ).style.display = "block";
+}
+
+function toBattle(){
+	var battleContainer = document.getElementById( "batlle-wrapper" ).style.display = "block";
+	var heroCreater = document.getElementById( "hero-creater" ).style.display = "none";
+	var inventoryWrapperContainer = document.getElementById( "inventory-wrapper" ).style.display = "none";
+	var shopWrapperContainer = document.getElementById( "shop-wrapper" ).style.display = "none";
+	var welcomeContainer = document.getElementById( "welcome" ).style.display = "none";
+}
+
+function toHome(){
+	var battleContainer = document.getElementById( "batlle-wrapper" ).style.display = "none";
+	var heroCreater = document.getElementById( "hero-creater" ).style.display = "none";
+	var inventoryWrapperContainer = document.getElementById( "inventory-wrapper" ).style.display = "none";
+	var shopWrapperContainer = document.getElementById( "shop-wrapper" ).style.display = "none";
+	var welcomeContainer = document.getElementById( "welcome" ).style.display = "none";
+}
+
+function toShop(){
+	var battleContainer = document.getElementById( "batlle-wrapper" ).style.display = "none";
+	var heroCreater = document.getElementById( "hero-creater" ).style.display = "none";
+	var inventoryWrapperContainer = document.getElementById( "inventory-wrapper" ).style.display = "none";
+	var shopWrapperContainer = document.getElementById( "shop-wrapper" ).style.display = "block";
+	var welcomeContainer = document.getElementById( "welcome" ).style.display = "none";
+}
